704b497ac219d3d4f4989575a2566c19bd9ca02e
Nurbek333 <nurbekuktamov333@gmail.com>
2024-05-09 10:43:37 +0500

    commit

--- portfol/models.py
+++ portfol/models.py
@@ -19,6 +19,7 @@ class Article(models.Model):
     is_active = models.BooleanField(default=False)
     image = models.ImageField(upload_to="Article/image")
     create_data = models.DateTimeField(auto_now=True)
+    github = models.CharField(max_length=70)
     #4ta maydon qo'shish
     def __str__(self) -> str:
         return f"{self.title}"
